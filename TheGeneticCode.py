0161756243403bf93a71a8f5886230b4752fb86d
Ermakov Petr <savef5@gmail.com>
2013-01-31 18:27:11 +0400

    Update TheGeneticCode.py

--- TheGeneticCode.py
+++ TheGeneticCode.py
@@ -5,16 +5,15 @@
 
 import sys
 
-intab =  ['UUU', 'CUU', 'AUU', 'GUU', 'UUC', 'CUC', 'AUC', 'GUC', 'UUA', 'CUA', 'AUA', 'GUA', 'UUG', 'CUG', 'AUG', 'GUG', 'UCU', 'CCU', 'ACU', 'GCU', 'UCC', 'CCC', 'ACC', 'GCC', 'UCA', 'CCA', 'ACA', 'GCA', 'UCG', 'CCG', 'ACG', 'GCG', 'UAU', 'CAU', 'AAU', 'GAU', 'UAC', 'CAC', 'AAC', 'GAC', 'UAA', 'CAA', 'AAA', 'GAA', 'UAG', 'CAG', 'AAG', 'GAG', 'UGU', 'CGU', 'AGU', 'GGU', 'UGC', 'CGC', 'AGC', 'GGC', 'UGA', 'CGA', 'AGA', 'GGA', 'UGG', 'CGG', 'AGG', 'GGG']
-outtab = ['F',   'L',   'I',   'V',   'F',   'L',   'I',   'V',   'L',   'L',   'I',   'V',   'L',   'L',   'M',   'V',   'S',   'P',   'T',   'A',   'S',   'P',   'T',   'A',   'S',   'P',   'T',   'A',   'S',   'P',   'T',   'A',   'Y',   'H',   'N',   'D',   'Y',   'H',   'N',   'D',   '$',   'Q',   'K',   'E',   '$',   'Q',   'K',   'E',   'C',   'R',   'S',   'G',   'C',   'R',   'S',   'G',   '$',   'R',   'R',   'G',   'W',   'R',   'R',   'G']
+keys =   ['UUU', 'CUU', 'AUU', 'GUU', 'UUC', 'CUC', 'AUC', 'GUC', 'UUA', 'CUA', 'AUA', 'GUA', 'UUG', 'CUG', 'AUG', 'GUG', 'UCU', 'CCU', 'ACU', 'GCU', 'UCC', 'CCC', 'ACC', 'GCC', 'UCA', 'CCA', 'ACA', 'GCA', 'UCG', 'CCG', 'ACG', 'GCG', 'UAU', 'CAU', 'AAU', 'GAU', 'UAC', 'CAC', 'AAC', 'GAC', 'UAA', 'CAA', 'AAA', 'GAA', 'UAG', 'CAG', 'AAG', 'GAG', 'UGU', 'CGU', 'AGU', 'GGU', 'UGC', 'CGC', 'AGC', 'GGC', 'UGA', 'CGA', 'AGA', 'GGA', 'UGG', 'CGG', 'AGG', 'GGG']
+values = ['F',   'L',   'I',   'V',   'F',   'L',   'I',   'V',   'L',   'L',   'I',   'V',   'L',   'L',   'M',   'V',   'S',   'P',   'T',   'A',   'S',   'P',   'T',   'A',   'S',   'P',   'T',   'A',   'S',   'P',   'T',   'A',   'Y',   'H',   'N',   'D',   'Y',   'H',   'N',   'D',   '$',   'Q',   'K',   'E',   '$',   'Q',   'K',   'E',   'C',   'R',   'S',   'G',   'C',   'R',   'S',   'G',   '$',   'R',   'R',   'G',   'W',   'R',   'R',   'G']
+dictionary = dict(zip(keys, values))
 
 file = open(sys.argv[1]).read()
-
 outline = ''
 
 for i in range(0, len(file), 3):
-  j = intab.index(file[i:i+3])
-	if outtab[j] == '$':
+	if dictionary[file[i:i+3]] == '$':
 		break
-	outline += outtab[j]
+	outline += dictionary[file[i:i+3]]
 print outline
